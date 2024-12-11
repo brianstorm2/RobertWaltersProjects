acd9d794dd222a2c4438c865a2556b957a6b7661
brianstorm2 <matt.j.parsons99@gmail.com>
2024-12-11 17:36:13 +0000

    Delete All Start Again

--- Login/src/MainLogin.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class MainLogin {
-    public static void main(String[] args) {
-        MainMenu mainMenu = new MainMenu();
-        mainMenu.initArray(); //initialises array, only want this happening once so new arrays can be added and retrieved
-        mainMenu.mainMenu(); //initialises main menu
-    }
-}
